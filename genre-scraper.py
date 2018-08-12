71d44e65926bb9efdba1d329de94e6c6440a85cb
Robbie Barrat <robbiebarrat@gmail.com>
2018-08-12 15:30:07 -0700

    Update genre-scraper.py

--- genre-scraper.py
+++ genre-scraper.py
@@ -1,192 +1,108 @@
-# For a list of stuff you can scrape - go to https://www.wikiart.org/en/paintings-by-genre/ and look at the categories there. Usually you'll need about 1000 images for a good GAN.
+# Updated/fixed version from Gene Kogan's "machine learning for artists" collection - ml4a.github.io
 
+import time
 import os
+import re
+import random
+import argparse
 import urllib
 import itertools
-
-
 import bs4
 from bs4 import BeautifulSoup
-
 import multiprocessing
 from multiprocessing.dummy import Pool
 
-# how many pages you want to scrape, if unsure, just put 1000 that'll get all of them.
-pages = 1000
+genre_list = ['portrait', 'landscape', 'genre-painting', 'abstract', 'religious-painting', 
+              'cityscape', 'sketch-and-study', 'figurative', 'illustration', 'still-life', 
+              'design', 'nude-painting-nu', 'mythological-painting', 'marina', 'animal-painting', 
+              'flower-painting', 'self-portrait', 'installation', 'photo', 'allegorical-painting', 
+              'history-painting', 'interior', 'literary-painting', 'poster', 'caricature', 
+              'battle-painting', 'wildlife-painting', 'cloudscape', 'miniature', 'veduta', 
+              'yakusha-e', 'calligraphy', 'graffiti', 'tessellation', 'capriccio', 'advertisement', 
+              'bird-and-flower-painting', 'performance', 'bijinga', 'pastorale', 'trompe-loeil', 
+              'vanitas', 'shan-shui', 'tapestry', 'mosaic', 'quadratura', 'panorama', 'architecture']
 
-# Change this value! 
+style_list = ['impressionism', 'realism', 'romanticism', 'expressionism', 
+            'post-impressionism', 'surrealism', 'art-nouveau', 'baroque', 
+            'symbolism', 'abstract-expressionism', 'na-ve-art-primitivism', 
+            'neoclassicism', 'cubism', 'rococo', 'northern-renaissance', 
+            'pop-art', 'minimalism', 'abstract-art', 'art-informel', 'ukiyo-e', 
+            'conceptual-art', 'color-field-painting', 'high-renaissance',
+            'mannerism-late-renaissance', 'neo-expressionism', 'early-renaissance', 
+            'magic-realism', 'academicism', 'op-art', 'lyrical-abstraction', 
+            'contemporary-realism', 'art-deco', 'fauvism', 'concretism', 
+            'ink-and-wash-painting', 'post-minimalism', 'social-realism', 
+            'hard-edge-painting', 'neo-romanticism', 'tachisme', 'pointillism', 
+            'socialist-realism', 'neo-pop-art']
 
-genre_to_scrape = "landscape"
+parser = argparse.ArgumentParser()
+parser.add_argument("--genre", help="which genre to scrape", choices=genre_list, default=None)
+parser.add_argument("--style", help="which style to scrape", choices=style_list, default=None)
+parser.add_argument("--num_pages", type=int, help="number of pages to scrape (leave blank to download all of them)", default=1000)
+parser.add_argument("--output_dir", help="where to put output files")
 
-# genre_to_scrape can be any one of the below values.
-"""
-portrait
-landscape
-genre-painting
-abstract
-religious-painting
-cityscape
-sketch-and-study
-figurative
-illustration
-still-life
-design
-nude-painting-nu
-mythological-painting
-marina
-animal-painting
-flower-painting
-self-portrait
-installation
-photo
-allegorical-painting
-history-painting
-"""
-# at this point - there might not be enough images for good results - but if you'd like to mix and match the images you're training and pull from multiple genres, go ahead
-"""
-interior
-literary-painting
-poster
-caricature
-battle-painting
-wildlife-painting
-cloudscape
-miniature
-veduta
-yakusha-e
-calligraphy
-graffiti
-tessellation
-capriccio
-advertisement
-bird-and-flower-painting
-performance
-bijinga
-pastorale
-trompe-loeil
-vanitas
-shan-shui
-tapestry
-mosaic
-quadratura
-panorama
-architecture
-"""
-
-
-
-# get list of all links to paintings of the specified genre
-def get_painting_list(count, genre=genre_to_scrape):
-	try:
-		url = "https://www.wikiart.org/en/paintings-by-genre/"+ genre+ "/" + str(count)
-		soup =  BeautifulSoup(urllib.request.urlopen(url), "lxml")
-		complete = 0
-		url_list = []
-		for item in str(soup.findAll()).split():
-			if item == "data" or complete == 1:
-				complete = 1
-				if "}];" in item:
-					break
-				if "https" in item:
-					link = "http" + item[6:-2]
-					url_list.append(link)
-					count += 1
-		return url_list
-	except Exception as e:
-		#print(e)
-		#print("Couldn't find page " + str(count) + " ... if you're seeing this at the beginning you're fine.")	
-		pass
-
-
-def downloader(link, genre):
-	
-	item,file = link	
-	name=file.split('/')
-	name_to_save_as = ''
-	
-	if len(name) == 5:
-		name_to_save_as = genre + "/" + "images/" + name[4] + "+" + name[5].split('.')[0] +".jpg"
-	if len(name) == 6:
-		name_to_save_as = genre + "/" + "images/" + name[4].split('.')[0]+".jpg"
-	if len(name) == 7:
-		name_to_save_as = genre + "/" + "images/" + name[5] + "+" + name[6].split('.')[0] +".jpg"
-
-	print(str(item) + " --- " + str(name_to_save_as))
-
-	try:
-	        urllib.request.urlretrieve(file,name_to_save_as)
-	except Exception as e:
-		print(e)
-		print("failed downloading " + str(name_to_save_as))	
-
-		
-
-def main(genre): 
-	pool_of_threads = Pool(multiprocessing.cpu_count() - 1) # lets hope you have more than 1 cpu core...
-
-	numbers = list(range(1,pages))
-
-	old_results = pool_of_threads.starmap( get_painting_list, zip( numbers, itertools.repeat(genre)) ) 
-	
-	pool_of_threads.close()
-	pool_of_threads.join()
-
-	results = []
-
-	for item in old_results:
-		if item:
-			for x in item:
-				results.append(x)
-
-	pool_of_threads = Pool(multiprocessing.cpu_count() - 1)
-	pool_of_threads.starmap(downloader, zip(enumerate(results), itertools.repeat(genre) ) )
-	pool_of_threads.close	
-	pool_of_threads.close()
-
-
-if not os.path.exists("./" + genre_to_scrape):
-	os.mkdir(genre_to_scrape)
-
-if not os.path.exists("./" + genre_to_scrape + "/images"):
-	os.mkdir(genre_to_scrape + "/images/")
-
-print("Building a list of all the paintings to download.. This may take a few minutes.")
-main(genre_to_scrape)
+num_downloaded = 0
+num_images = 0
 
 
 
 
+def get_painting_list(count, typep, searchword):
+    try:
+        time.sleep(3.0*random.random())  # random sleep to decrease concurrence of requests
+        url = "https://www.wikiart.org/en/paintings-by-%s/%s/%d"%(typep, searchword, count)
+        soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
+        regex = r'https?://uploads[0-9]+[^/\s]+/\S+\.jpg'
+        url_list = re.findall(regex, str(soup.html()))
+        count += len(url_list)
+        return url_list
+    except Exception as e:
+        print('failed to scrape %s'%url, e)
 
 
+def downloader(link, genre, output_dir):
+    global num_downloaded, num_images
+    item, file = link
+    filepath = file.split('/')
+    #savepath = '%s/%s/%d_%s' % (output_dir, genre, item, filepath[-1])
+    savepath = '%s/%s/%s' % (output_dir, genre, filepath[-1])    
+    try:
+        time.sleep(0.2)  # try not to get a 403
+        urllib.request.urlretrieve(file, savepath)
+        num_downloaded += 1
+        if num_downloaded % 100 == 0:
+            print('downloaded number %d / %d...' % (num_downloaded, num_images))
+    except Exception as e:
+        print("failed downloading " + str(file), e) 
 
 
+def main(typep, searchword, num_pages, output_dir):
+    global num_images
+    print('gathering links to images... this may take a few minutes')
+    threadpool = Pool(multiprocessing.cpu_count()-1)
+    numbers = list(range(1, num_pages))
+    wikiart_pages = threadpool.starmap(get_painting_list, zip(numbers, itertools.repeat(typep), itertools.repeat(searchword))) 
+    threadpool.close()
+    threadpool.join()
+
+    pages = [page for page in wikiart_pages if page ]
+    items = [item for sublist in pages for item in sublist]
+    items = list(set(items))  # get rid of duplicates
+    num_images = len(items)
+    
+    if not os.path.isdir('%s/%s'%(output_dir, searchword)):
+        os.mkdir('%s/%s'%(output_dir, searchword))
+    
+    print('attempting to download %d images'%num_images)
+    threadpool = Pool(multiprocessing.cpu_count()-1)
+    threadpool.starmap(downloader, zip(enumerate(items), itertools.repeat(searchword), itertools.repeat(output_dir)))
+    threadpool.close    
+    threadpool.close()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
+if __name__ == '__main__':
+    args = parser.parse_args()
+    searchword, typep = (args.genre, 'genre') if args.genre is not None else (args.style, 'style')
+    num_pages = args.num_pages
+    output_dir = args.output_dir
+    main(typep, searchword, num_pages, output_dir)
